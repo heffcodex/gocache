8d536a033fc1d0a92220f162d6fadfe5fe82d678
heffcodex <dev@heffcodex.pro>
2021-03-25 19:40:24 +0300

    suppress redis.Nil err

--- redis_cache.go
+++ redis_cache.go
@@ -18,15 +18,21 @@ func NewRedisCache(c *redis.Client) Cacher {
 }
 
 func (c *RedisCache) GetScan(key string, v interface{}) (err error) {
-	return c.c.Get(c.key(key)).Scan(v)
+	return c.wrapErr(c.c.Get(c.key(key)).Scan(v))
 }
 
 func (c *RedisCache) GetInt64(key string) (value int64, err error) {
-	return c.c.Get(c.key(key)).Int64()
+	value, err = c.c.Get(c.key(key)).Int64()
+	err = c.wrapErr(err)
+
+	return
 }
 
 func (c *RedisCache) GetString(key string) (value string, err error) {
-	return c.c.Get(c.key(key)).Result()
+	value, err = c.c.Get(c.key(key)).Result()
+	err = c.wrapErr(err)
+
+	return
 }
 
 func (c *RedisCache) Set(key string, value interface{}, ttl time.Duration) (err error) {
@@ -34,11 +40,11 @@ func (c *RedisCache) Set(key string, value interface{}, ttl time.Duration) (err 
 }
 
 func (c *RedisCache) Del(key string) (err error) {
-	return c.c.Del(key).Err()
+	return c.wrapErr(c.c.Del(key).Err())
 }
 
 func (c *RedisCache) RememberInt64(key string, f RememberInt64Func, ttl time.Duration) (value int64, err error) {
-	value, err = c.GetInt64(key)
+	value, err = c.c.Get(c.key(key)).Int64()
 	if err != nil {
 		value, err = f()
 		if err != nil {
@@ -55,7 +61,7 @@ func (c *RedisCache) RememberInt64(key string, f RememberInt64Func, ttl time.Dur
 }
 
 func (c *RedisCache) RememberString(key string, f RememberStringFunc, ttl time.Duration) (value string, err error) {
-	value, err = c.GetString(key)
+	value, err = c.c.Get(c.key(key)).Result()
 	if err != nil {
 		value, err = f()
 		if err != nil {
@@ -80,4 +86,12 @@ func (c *RedisCache) WithPrefix(prefix string) Cacher {
 
 func (c *RedisCache) key(key string) string {
 	return c.prefix + key
+}
+
+func (c *RedisCache) wrapErr(err error) error {
+	if err == redis.Nil {
+		err = nil
+	}
+
+	return err
 }
